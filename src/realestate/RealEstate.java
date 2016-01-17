a244f733ea93cbb3939f86bf46458adf1d0ede33
duongnartist <duongnartist@gmail.com>
2016-01-18 04:52:50 +0700

    init commit

---
+++ src/realestate/RealEstate.java
@@ -0,0 +1,25 @@
+package realestate;
+
+import realestate.controllers.UrlParser;
+import realestate.models.Category;
+import realestate.models.Group;
+import realestate.models.Page;
+import realestate.models.Type;
+
+/**
+ * Created by duongnartist on 1/18/16.
+ */
+public class RealEstate {
+
+    public static final String HOME = "res/realestate/";
+
+    public static void main(String[] args) {
+        Page.init();
+        Type.init();
+        Category.init();
+        Group.init();
+        for (Group group : Group.getGroups()) {
+            UrlParser.initMainUrls(group);
+        }
+    }
+}
